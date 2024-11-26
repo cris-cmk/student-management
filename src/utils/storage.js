5b3ac61b06d2d47722bdc35f909b9a0c38d75454
MonicahMackena¨ <mackenamuthui@gmail.com>
2024-11-26 19:39:27 +0300

    updated storage util

---
+++ src/utils/storage.js
@@ -0,0 +1,9 @@
+export const getStudentsFromLocalStorage = () => {
+    const data = localStorage.getItem('students');
+    return data ? JSON.parse(data) : [];
+  };
+  
+  export const saveStudentsToLocalStorage = (students) => {
+    localStorage.setItem('students', JSON.stringify(students));
+  };
+  
